4b1ff45fbc24cf5966e59741bc3b1e0e9d58dc44
Nate Bass <nate.bass@outlook.com>
2025-09-03 06:24:56 -0700

    Add a numbered timeline to the homepage.

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import AppLayout from "../../components/layout/AppLayout";
 import "./home.css";
+import HomeNumbered2 from "@/components/layout/HomeNumbered/HomeNumbered2";
 
 export default function HomePage() {
   return (
@@ -114,75 +115,11 @@ export default function HomePage() {
           </div>
         </section>
         <section className="mx-auto max-w-[calc(var(--screen-xxl)_+_80px)] px-10">
-          <h1 className="heading-section app-color--dark">Opportunities</h1>
-          <h2 className="sub-heading">Explore Our Volunteer Options</h2>
-          <div className="home-numbered-grid">
-            <div className="home-numbered-container">
-              <Image
-                src="/img/1.png"
-                alt="Open Data Day"
-                width={81}
-                height={80}
-              />
-              <h2 className="home-numbered-header sm:h3-bold h4-bold">
-                Engage with Our Community
-              </h2>
-              <p className="home-numbered-paragraph p3-regular">
-                Connect with like-minded individuals, share ideas, and
-                collaborate on projects at our meetups. Join us to be a part of
-                a vibrant community dedicated to positive change through
-                technology.
-              </p>
-              <Button
-                className="azbtn azbtn-alt-gold dark-button-transition"
-                href="https://www.meetup.com/openfresno/"
-                target="_blank"
-                textContent="Visit Meetup"
-              />
-            </div>
-            <div className="home-numbered-container">
-              <Image
-                src="/img/2.png"
-                alt="Open Data Day"
-                width={80}
-                height={80}
-              />
-              <h2 className="home-numbered-header sm:h3-bold h4-bold">
-                Drive Innovation with Projects
-              </h2>
-              <p className="home-numbered-paragraph p3-regular">
-                Discover how you can contribute your skills to projects that
-                address real challenges and enhance our city. Be part of a
-                dynamic team working on solutions that make a difference.
-              </p>
-              <Button
-                className="azbtn azbtn-alt-gold dark-button-transition"
-                href="/projects"
-                textContent="See Our Projects"
-              />
-            </div>
-            <div className="home-numbered-container">
-              <Image
-                src="/img/3.png"
-                alt="Open Data Day"
-                width={81}
-                height={80}
-              />
-              <h2 className="home-numbered-header sm:h3-bold h4-bold">
-                Pitch Your Vision
-              </h2>
-              <p className="home-numbered-paragraph p3-regular">
-                Have a project idea that can benefit the community? Pitch it to
-                us and join forces with our community of innovators to bring
-                your vision to life, driving positive change in Fresno.
-              </p>
-              <Button
-                className="azbtn azbtn-alt-gold dark-button-transition"
-                href="/pitch"
-                textContent="Pitch a Project"
-              />
-            </div>
+          <div className="mb-28 hidden md:block">
+            <h1 className="heading-section app-color--dark">Opportunities</h1>
+            <h2 className="sub-heading">Explore Our Volunteer Options</h2>
           </div>
+          <HomeNumbered2 />
         </section>
         <section className="general-section-light">
           <div className="general-section-container heading-underline">
---
+++ src/components/layout/HomeNumbered/HomeNumbered2.jsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import "./home-numbered2.css";
+
+export default function HomeNumbered2() {
+  return (
+    <div className="timeline p3-regular">
+      <div className="mt-10 ps-8 pb-10 md:hidden">
+        <h1 className="heading-section app-color--dark">Opportunities</h1>
+        <h2 className="h4-semi-bold mt-[10px]">
+          Explore Our Volunteer Options
+        </h2>
+      </div>
+      <div className="timeline-item">
+        <div className="timeline-blue-circle">1</div>
+        <div className="content">
+          <h2 className="medium-heading-title">Engage with Our Community</h2>
+          <p className="mt-4 mb-8">
+            Connect with like-minded individuals, share ideas, and collaborate
+            on projects at our meetups.
+            <br />
+            Join us to be a part of a vibrant community dedicated to positive
+            change through technology.
+          </p>
+          <Link className="azbtn azbtn-blue button-regular" href="">
+            Visit Meetup
+          </Link>
+        </div>
+      </div>
+      <div className="timeline-item">
+        <div className="timeline-blue-circle">2</div>
+        <div className="content">
+          <h2 className="medium-heading-title">
+            Drive Innovation with Projects
+          </h2>
+          <p className="mt-4 mb-8">
+            Discover how you can contribute your skills to projects that address
+            real challenges and enhance our city. Be part of a dynamic team
+            working on solutions that make a difference.
+          </p>
+          <Link className="azbtn azbtn-blue button-regular" href="">
+            See Our Project
+          </Link>
+        </div>
+      </div>
+      <div className="timeline-item">
+        <div className="timeline-blue-circle">3</div>
+        <div className="content">
+          <h2 className="medium-heading-title">Pitch Your Vision</h2>
+          <p className="mt-4 mb-8">
+            Have a project idea that can benefit the community? Pitch it to us
+            and join forces with our community of innovators to bring your
+            vision to life, driving positive change in Central California.
+          </p>
+          <Link className="azbtn azbtn-blue button-regular" href="">
+            Pitch a Project
+          </Link>
+        </div>
+      </div>
+      <div className="timeline-item">
+        <div className="timeline-blue-circle">4</div>
+        <div className="content">
+          <h2 className="medium-heading-title">
+            Explore On-Site Opportunities with Root Access
+          </h2>
+          <p className="mt-4 mb-8">
+            Discover Root Access on Van Ness Ave in the Tower District, just a
+            block south of Fresno City College. From advanced 3D printers and a
+            cozy lounge to laser cutting, workshops, and an electronics haven,
+            explore what awaits you at our partner's space!{" "}
+          </p>
+          <Link className="azbtn azbtn-blue button-regular" href="">
+            Check It Out
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
