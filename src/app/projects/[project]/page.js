cc4994bf9c81372e3f9e99e9cbe696c16399afc3
Nate Bass <nw3455@gmail.com>
2025-09-09 15:50:12 -0700

    TEMPORARY: Comment out project lists due to build error.
    Build error occurred
    [Error: Page "/projects/[project]" is missing "generateStaticParams()" so it cannot be used with "output: export" config.]
    
    This error means that Next.js static export mode requires all dynamic routes (like /projects/[project]) to have a working generateStaticParams function that returns a list of all possible params at build time. However, your current implementation fetches data from the GitHub API at build time, which may not work with static export (especially if the API is unavailable or rate-limited).
    
    To fix this for static export, you must provide a static list of project params, or read them from a local file, not from an external API.

--- src/app/projects/[project]/page.js
+++ src/app/projects/[project]/page.js
@@ -1,25 +1,30 @@
 import { githubOwner } from "../../../utils/constants/app-data";
 import SingleProject from "./singleProject";
 import AppNavbar from "../../../components/layout/navbar/AppNavbar";
-import MarginTop from "../../../components/layout/MarginTop";
+// import MarginTop from "../../../components/layout/MarginTop";
 import AppFooter from "../../../components/layout/footer/AppFooter";
 
 // See https://nextjs.org/docs/app/api-reference/functions/generate-static-params
 export async function generateStaticParams() {
-  const ghResponses = await fetch(
-    `https://api.github.com/orgs/${githubOwner}/repos?per_page=20&sort=updated&direction=desc`,
-  ).then((res) => res.json());
+  // const ghResponses = await fetch(
+  //   `https://api.github.com/orgs/${githubOwner}/repos?per_page=20&sort=updated&direction=desc`,
+  // ).then((res) => res.json());
 
-  return ghResponses.map((ghResponse) => ({
-    project: ghResponse.name,
-  }));
+  // return ghResponses.map((ghResponse) => ({
+  //   project: ghResponse.name,
+  // }));
+  return [
+    { project: "project1" },
+    { project: "project2" },
+    { project: "project3" },
+  ];
 }
 
 export default function Page({ params }) {
   return (
     <>
       <AppNavbar fade={false} />
-      <MarginTop />
+      {/* <MarginTop /> */}
       <SingleProject
         githubFullName={`${githubOwner}/${params.project}`}
       ></SingleProject>
