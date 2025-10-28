0d9125b59e1b539a9eac5b9ae562720f970cc7bb
ZOrangeBandit <jampmcdole@gmail.com>
2025-10-28 16:10:17 -0700

    Fixed Timeline Item 4's content regression.

--- src/components/ui/timeline/Timeline.jsx
+++ src/components/ui/timeline/Timeline.jsx
@@ -98,9 +98,10 @@ export default function Timeline({ className }) {
         ]}
         updateTimelineNumbers={updateTimelineNumbers}
       >
-        Have a project idea that can benefit the community? Pitch it to us and
-        join forces with our community of innovators to bring your vision to
-        life, driving positive change in Central California.
+        Discover Root Access on Van Ness Ave in the Tower District, just a block
+        south of Fresno City College. From advanced 3D printers and a cozy
+        lounge to laser cutting, workshops, and an electronics haven, <b>explore
+        what awaits you at our partner's space!</b>
       </TimelineItem>
       <style>
         {((_timelineNumbers, _clientRect) => {
