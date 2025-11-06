5aa5d61f0574e77beb7b2c388d7960d2ee8db486
ZOrangeBandit <jampmcdole@gmail.com>
2025-11-05 18:02:28 -0800

    Themed the SimpleDialog component
    * Created light, dark and gray material themes in MaterialTheme
    * Modified SimpleDialog to use SectionType to update its theming

--- src/components/ui/SimpleDialog.jsx
+++ src/components/ui/SimpleDialog.jsx
@@ -1,18 +1,24 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import { SectionType } from "@/utility/constants/theme";
+import { themes } from "@/components/ui/MaterialTheme";
+import { ThemeProvider } from "@mui/material";
 
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root": {
-    padding: theme.spacing(2)
-  }
-}));
-
+/**
+ * @component
+ * @param {object} [props]
+ * @param {string} [props.title] The title of the Dialog
+ * @param {JSX.Element} [props.children] The contents of the dialog
+ * @param {boolean} [props.openState] The state of the Dialog
+ * @param {()=>null} [props.handleClose] The that sets openState to false
+ * @param {SectionType} [props.sectionType]
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function SimpleDialog({
                                        title = "",
                                        children = (<></>),
@@ -23,30 +29,38 @@ export default function SimpleDialog({
                                        ...props
                                      }) {
   return (
-    <BootstrapDialog
-      onClose={handleClose}
-      aria-labelledby="customized-dialog-title"
-      open={openState}
-      {...props}
-    >
-      <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-        {title}
-      </DialogTitle>
-      <IconButton
-        aria-label="close"
-        onClick={handleClose}
-        sx={(theme) => ({
-          position: "absolute",
-          right: 8,
-          top: 8,
-          color: theme.palette.grey[500]
-        })}
+    <ThemeProvider theme={themes[sectionType]}>
+      <Dialog
+        className={"Test"}
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={openState}
+        sx={{
+          "& .MuiPaper-root": {
+            "--Paper-overlay": "none!important"
+          }
+        }}
+        {...props}
       >
-        <CloseIcon />
-      </IconButton>
-      <DialogContent>
-        {children}
-      </DialogContent>
-    </BootstrapDialog>
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          <span className={`sub-heading-main`}>{title}</span>
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={(theme) => ({
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: theme.palette.grey[500]
+          })}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent>
+          {children}
+        </DialogContent>
+      </Dialog>
+    </ThemeProvider>
   );
 }
