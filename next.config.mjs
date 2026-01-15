82530c8926ab87acf9967e9544d25482b32a9e6e
Nate Bass <nate.bass@outlook.com>
2026-01-15 15:40:04 -0800

    Refactor the Next.js configuration file.

---
+++ next.config.mjs
@@ -0,0 +1,26 @@
+import getBasePath from "./src/integrations/gh-pages/getBasePath.mjs";
+import createMDX from "@next/mdx";
+
+const basePath = getBasePath();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath,
+  assetPrefix: basePath,
+  images: {
+    unoptimized: true,
+  },
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
+  reactCompiler: true,
+};
+
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+});
+
+export default withMDX(nextConfig);
+
---
+++ src/integrations/gh-pages/getBasePath.mjs
@@ -0,0 +1,37 @@
+/**
+ * Get a relative base path for the website assets when deploying to a fork on GitHub Pages. This only affects forks. Even if it is not configured properly, images will still work correctly in development and production (empty string base path).
+ *
+ * This file must have a .mjs because it is used in next.config.mjs.
+ *
+ * ### **Important!** Set environment variables in the "Build with Next.js" step in GitHub Actions.
+ *
+ * - GITHUB_ACTIONS: Whether the app is running in a GitHub Actions environment.
+ * - GITHUB_REPOSITORY: The repository name in the format "owner/repo".
+ *
+ * ### Example base paths.
+ *
+ * - **If it is a fork:** fe-openfresno.org-doc.
+ * - **If it is the main repository:** Empty string.
+ * - **Custom domain:** Empty string.
+ * - **Local development:** Empty string.
+ *
+ * @param {boolean} repositoryCustomDomain - Whether the main repository uses a custom domain. If true, the base path will be an empty string.
+ * @param {string} repositoryName - The name of the main repository, in the format "owner/repo".
+ * @returns {string} - The base path of the website URL.
+ */
+export default function getBasePath(
+  repositoryCustomDomain = false,
+  repositoryName = "openfresno/fe-openfresno.org-doc",
+) {
+  const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+  const isMainRepo = process.env.GITHUB_REPOSITORY === repositoryName;
+  if (isGitHubActions) {
+    if (isMainRepo && repositoryCustomDomain) {
+      return "";
+    } else {
+      return `/${repositoryName.split("/")[1]}`;
+    }
+  } else {
+    return "";
+  }
+}
