a808fad59fe1bef3f92d03d3e11c886ea7b7f34b
Nate Bass <nate.bass@outlook.com>
2026-01-15 19:25:45 -0800

    Refactor the project configuration files.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,8 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { defineConfig } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-  ...compat.extends("next/core-web-vitals"),
-];
+const eslintConfig = defineConfig([
+  ...nextVitals,
+]);
 
 export default eslintConfig;
