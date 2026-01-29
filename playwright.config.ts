d8dd8fb002f81a2f55aea9598fc43c466e6f156f
Nate Bass <nate.bass@outlook.com>
2026-01-29 13:36:15 -0800

    Fix a timeout error in the Playwright GitHub Action.
    * Upgrade all node modules.

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,8 +27,8 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run dev",
-    url: "http://localhost:3000",
+    command: process.env.CI ? 'npm run build && npm run start' : 'npm run dev',
+    url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
